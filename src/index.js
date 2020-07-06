e64a94c2414dd70876a40465f8deb5835d3e5ccd
Stéphane Beaudet <stephaneBlitz@gmail.com>
2020-07-06 15:36:59 -0400

    again trying to setup for Heroku

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ var http = require("http");
 http.createServer(function(req, res) {
     res.write("HOOK!!"); //write a response to the client
     res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(process.env.PORT); //the server object listens on port 8080
